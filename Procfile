75f632c883de7866a9b2ca2a11f05229a41bec73
John Keyes <john@keyes.ie>
2014-06-06 15:26:03 +0100

    Updating baseline background to use latest Django.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python overlay/manage.py run_gunicorn -b "0.0.0.0:$PORT" -w 3
+web: gunicorn overlay.wsgi:application -k gevent -b 0.0.0.0:$PORT -w 3
--- overlay/urls.py
+++ overlay/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'overlay.views.overlay', name='home'),
+    url(r'^$', 'overlay.views.overlay', name='home')
 )
